78b1c50bc323e88d06cec134fd4240cf25038ff0
Tom <tomruiz2611@gmail.com>
2022-04-28 18:19:13 +0200

    création de la structure de la table pour le header

--- ws-dragons-donjon/client/src/components/Table.jsx
+++ ws-dragons-donjon/client/src/components/Table.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import Body from './Body'
+import '../style/table.scss'
 
 export default function Table() {
     return (
         <>
+            <div>Header</div>
+            <table className='table-main'>
+                <tr className='table-head'>
+                    <td>Key</td>
+                    <td>Value</td>
+                    <td>Description</td>
+                </tr>
+                <tr>
+                    <td>key1</td>
+                    <td>value1</td>
+                    <td>desc1</td>
+                </tr>
 
-            <div>Table</div>
+            </table>
             {/* // <Params /> */}
             <Body />
             {/* // <Headers /> */}
