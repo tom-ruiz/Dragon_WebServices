54307657c4664a4b919620f55d9d48ccff5f1770
samir-massoundi <samir.j7.massoundi@gmail.com>
2022-04-29 14:47:28 +0200

    ajout d'un liste pour la présentation

--- ws-dragons-donjon/client/src/components/Response.jsx
+++ ws-dragons-donjon/client/src/components/Response.jsx
@@ -1,29 +1,35 @@
 import React from 'react'
 
-export default function Response({response}) {
-    if(response !== "reponse is undefined"){
-        if(response.presentation && response.carte){
-            for (var [cle, valeur] of Object.entries(response)){
-                console.log(cle + ' ' + valeur);
-                return(
+export default function Response({ response }) {
+    if (response !== "reponse is undefined") {
+        if (response.presentation && response.carte) {
+            for (var [cle, valeur] of Object.entries(response)) {
+                return (
                     <>
-                    <div>Response</div><div>
-                        <pre>
-                            <div>
-                                <p>{valeur}</p>
-                            </div>
-                        </pre> 
-                    </div>
+                        <div>Response</div><div>
+                            <pre>
+                                {JSON.stringify(response.presentation, null, 2)}
+
+                            </pre>
+                            <pre>
+                                <ul>
+                                    {response.carte.map(location => {
+                                        return <li>{location}</li>
+                                    })}
+                                </ul>
+                            </pre>
+                        </div>
+
                     </>
                 )
-              }
-        }else{
+            }
+        } else {
             return (
                 <>
                     <div>Response</div><div>
                         <pre>
-                            {JSON.stringify(response,null,2)}
-                        </pre> 
+                            {JSON.stringify(response, null, 2)}
+                        </pre>
                     </div>
                 </>
             )
